ddce180416f869077c63c5444d1306a6175ae110
Omar <omartsadek@gmail.com>
2022-03-28 10:30:35 -0400

    commit tree 03d050e929d3155decfed22350b29cc77c013e31

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.`;
+}
+function introductionWithLanguage(Aki,Ember){
+    return  `Hi, my name is ${Aki} and I am learning to program in ${Ember}.`;
+}
+
+
+
+function introductionWithLanguageOptional(name,language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+
